c95f381447814c67f6323ea477d90cf41fa5977b
Roger Florat <rflorat@cidiverte.it>
2018-01-19 10:31:27 +0100

    Using Alamofire to Download Images / Cancelling All Sessions

--- PixelCity/PixelCity/Controller/MapVC.swift
+++ PixelCity/PixelCity/Controller/MapVC.swift
@@ -9,6 +9,8 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Alamofire
+import AlamofireImage
 
 class MapVC: UIViewController, UIGestureRecognizerDelegate {
     
@@ -27,6 +29,9 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     
     var flowLayout = UICollectionViewFlowLayout()
     var collectionView : UICollectionView?
+    
+    var imageUrlArray = [String]()
+   var imageArray = [UIImage]()
     
     // Constants
     let regionRadius : Double = 1000
@@ -67,6 +72,7 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     }
     
     func animateViewUp(){
+        cancelAllSessions()
         pullUpViewHeightConstraints.constant = 300
         UIView.animate(withDuration: 0.3) {
             self.view.layoutIfNeeded()
@@ -148,6 +154,7 @@ extension MapVC: MKMapViewDelegate {
         removePin()
         removeSpiner()
         removeProgressLbl()
+        cancelAllSessions()
         
         animateViewUp()
         
@@ -163,11 +170,73 @@ extension MapVC: MKMapViewDelegate {
         
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(touchCoordinate, regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
+        
+        retrieveUrls(forAnnotation: annotation) { (finished) in
+            
+            if finished {
+                self.retrieveImages(handler: { (finished) in
+                    if finished {
+                        self.retrieveImages(handler: { (finished) in
+                            if finished {
+                                self.removeSpiner()
+                                self.removeProgressLbl()
+                            }
+                        })
+                    }
+                })
+            }
+            
+        }
     }
     
     func removePin() {
         for annotation in mapView.annotations {
             mapView.removeAnnotation(annotation)
+        }
+    }
+    
+    func retrieveUrls(forAnnotation annotation: DroppablePin, handler: @escaping(_ status: Bool) -> ()) {
+        imageUrlArray = []
+        
+        Alamofire.request(flickrUrl(forApiKey: API_KEY, withAnnotation: annotation, andNumberOfPhotos: 40)).responseJSON { (response) in
+
+            guard let json = response.result.value as? Dictionary<String, AnyObject> else { return }
+            
+            let photosDic = json["photos"] as? Dictionary<String, AnyObject>
+            let photosDictArray = photosDic!["photo"] as? [Dictionary<String, AnyObject>]
+           
+            for photo in photosDictArray! {
+                let postUrl = "https://farm\(photo["farm"]!).staticflickr.com/\(photo["server"]!)/\(photo["id"]!)_\(photo["secret"]!)_h_d.jpg"
+                
+                self.imageUrlArray.append(postUrl)
+            }
+            handler(true)
+        }
+        
+    }
+    
+    
+    func retrieveImages(handler: @escaping(_ status: Bool) -> ()) {
+        imageArray = []
+        
+        for url in imageUrlArray {
+            Alamofire.request(url).responseImage(completionHandler: { (response) in
+                guard let image = response.result.value else { return }
+                self.imageArray.append(image)
+                self.progressLbl?.text = "\(self.imageArray.count)/40 IMAGES DOWNLOADED"
+                
+                if self.imageArray.count == self.imageArray.count {
+                    handler(true)
+                }
+            })
+        }
+    }
+    
+    func cancelAllSessions(){
+        Alamofire.SessionManager.default.session.getTasksWithCompletionHandler { (sessionDataTask
+            , uploadData, downloadData) in
+            sessionDataTask.forEach({ $0.cancel() })
+            downloadData.forEach({ $0.cancel() })
         }
     }
     
